83f147734296e3896f23613ddfe3f8b761d5b937
J Duane <69173891+DuaneIndustries@users.noreply.github.com>
2025-01-22 18:59:29 -0500

    Update app.py
    v15

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from datetime import datetime, timedelta, date
 # sheet_name = "Data”
 # url = f”https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-url='https://raw.githubusercontent.com/DuaneIndustries/colonialgantt/main/Colonial_Coffee_Gantt_14.csv'
+url='https://raw.githubusercontent.com/DuaneIndustries/colonialgantt/main/Colonial_Coffee_Gantt_15.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
@@ -36,7 +36,7 @@ df = df.sort_values(by='Start Date',ascending=False)
 dff = df
 
 # Determine the start of each week
-start_dates = pd.date_range(start='2025-02-16', end='2025-04-30', freq='W-MON')
+start_dates = pd.date_range(start='2025-01-27', end='2025-04-30', freq='W-MON')
 
 # Create a DataFrame with start dates of each week
 week_markers = pd.DataFrame({'Start Date': start_dates, 'Week_Start': True})
@@ -61,7 +61,7 @@ app.layout = html.Div([
                              id='section-dropdown'),
         dcc.DatePickerRange(
             id='date-picker-range',
-            start_date=date(2025,2,1),
+            start_date=date(2025,1,27),
             end_date=date(2025,4,30),
             style={'display': 'inline-block', 'float': 'right'}
         ),
